5694cda6ca56f5beb1bfe90af917db1a07d2ef4f
wojciech-kniec <kniecwojciech@gmail.com>
2019-04-08 10:33:07 +0200

    init

--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -1,6 +1,10 @@
+import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
 import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,10 +15,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 //@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
-//    @Mock
+    //    @Mock
     private UserDao userDao;
 
-//    @InjectMocks
+    //    @InjectMocks
     private UserService userService; //= new UserService(userDao);
 
     @BeforeEach
@@ -34,12 +38,37 @@ public class UserServiceTest {
 
     @Test
     public void shouldGetAllUsers() {
-        // Zadanie 1
+        // given
+        List<User> users = Arrays.asList(
+                new User("user1"), new User("user2"), new User("user3"), new User("user4"));
+        when(userDao.getAllUsers()).thenReturn(users);
+        // when
+        List<User> result = userService.getAllUsers();
+        // then
+        assertThat(result).hasSize(4);
+
     }
 
     @Test
     public void shouldReturnAllUsersMatchingPattern() {
-        // Zadanie 2
+        // given
+        List<User> allUsers = List.of(
+                new User("Kowalski"),
+                new User("Nowak"),
+                new User("ABS"),
+                new User("aweawe"));
+
+        when(userDao.getAllUsers()).thenReturn(allUsers);
+
+
+        // when
+        List<User> result = userService.findUsers("ow");
+        // then
+        assertThat(result).hasSize(2)
+                .contains(
+                        new User("Kowalski"),
+                        new User("Nowak")
+                );
     }
 
     @Test
