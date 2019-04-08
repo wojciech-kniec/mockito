1ff48c8243c3ff00a6de3bcc3269aceb2f5ec966
Michal Nowakowski <michal@nowakowski.me.uk>
2019-04-08 09:47:27 +0200

    Zadanie 1

--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -1,17 +1,36 @@
+import static org.mockito.Mockito.mock;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
+//@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
-    @Mock
+//    @Mock
     private UserDao userDao;
 
-    @InjectMocks
-    private UserService userService;
+//    @InjectMocks
+    private UserService userService; //= new UserService(userDao);
+
+    @BeforeEach
+    public void setUp() {
+        userDao = mock(UserDao.class);
+        userService = new UserService(userDao);
+    }
+
+    @Test
+    public void returnFromMock() {
+        User user = userDao.findUser("jan");
+        System.out.println(user);
+
+        List<User> users = userService.getAllUsers();
+        System.out.println(users);
+    }
 
     @Test
     public void shouldGetAllUsers() {
